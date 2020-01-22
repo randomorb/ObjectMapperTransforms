2627604d191bc2dab50ddb5d16b1a5a0e35e472b
Brandon Bro <>
2020-01-21 16:40:26 -0800

    - migrates to carthage from cocopods. - adds framrwork

---
+++ TRLObjectMapperTransforms/TRLObjectMapperTransforms.h
@@ -0,0 +1,19 @@
+//
+//  TRLObjectMapperTransforms.h
+//  TRLObjectMapperTransforms
+//
+//  Created by Brandon Bro on 1/21/20.
+//  Copyright © 2020 Ftkey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TRLObjectMapperTransforms.
+FOUNDATION_EXPORT double TRLObjectMapperTransformsVersionNumber;
+
+//! Project version string for TRLObjectMapperTransforms.
+FOUNDATION_EXPORT const unsigned char TRLObjectMapperTransformsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TRLObjectMapperTransforms/PublicHeader.h>
+
+
